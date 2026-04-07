d69b74b5ad6a4876b2b1c7ee29e04300496f758c
Fatih Çokyüksel <fatihcokyuksel@gmail.com>
2026-04-07 03:14:56 +0300

    update

--- yolo_trainer.py
+++ yolo_trainer.py
@@ -1,8 +1,13 @@
 from ultralytics import YOLO
+import time
+from google.colab import files
 
 def train(model_size="yolo11s.pt", epochs=50, image_size=640, batch_size=16, project_name="yolo_training"):
     model = YOLO(model_size)
-    model.train(
+
+    start_time = time.time()
+
+    results = model.train(
         data="/content/data.yaml",
         epochs=epochs,
         imgsz=image_size,
@@ -11,9 +16,20 @@ def train(model_size="yolo11s.pt", epochs=50, image_size=640, batch_size=16, pro
         name=project_name
     )
 
-    print("\nEğitim Tamamlandı!")
-    print(f"En iyi model: /content/runs/{project_name}/weights/best.pt")
+    elapsed = time.time() - start_time
+    hours = int(elapsed // 3600)
+    minutes = int((elapsed % 3600) // 60)
+    seconds = int(elapsed % 60)
 
+    best_mAP = results.results_dict.get("metrics/mAP50(B)", 0)
 
+    print("\n" + "="*50)
+    print(f"Eğitim Tamamlandı! \nEğitim süresi: {hours}sa {minutes}dk {seconds}sn")
+    print(f"En iyi mAP@50: {best_mAP:.4f}")
+    print(f"En iyi modelin konumu: /content/runs/{project_name}/weights/best.py")
+    print("\n" + "="*50)
+
+    print("\nModel indiriliyor...")
+    files.download(f"/content/runs/{project_name}/weights/best.pt")
 
 
